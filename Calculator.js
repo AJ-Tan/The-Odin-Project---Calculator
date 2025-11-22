23ae05b1426814a53c92440f22cd961e13f76828
AJ-Tan <aldomin22@gmail.com>
2025-11-22 20:04:13 +0800

    Added window event listener 'keydown'

--- Calculator.js
+++ Calculator.js
@@ -5,50 +5,16 @@ export class Calculator {
     this.#op = null;
   }
 
-  methods(op, a, b = null) {
-    switch(op) {
-      case 'mod':
-        return a % b;
-      case '**':
-        return a ** a;
-      case '+':
-        return a + b;
-      case '-':
-        return a - b;
-      case '*':
-        return a * b;
-      case '÷':
-        return a / b;
-    }
+  #methods(op, a, b) {
+    return {
+      'mod': a % b,
+      '**': a ** b,
+      '+': a + b,
+      '-': a - b,
+      '*': a * b,
+      '/': a / b
+    }[op]
   } 
-
-  initialize() {
-    const calcButtons = document.querySelectorAll('button');
-    const calcInput = document.getElementById('calc-input');
-
-    calcButtons.forEach(btn => {
-      btn.addEventListener('click', (e) => {
-        const inputValue = e.currentTarget.dataset['input'];
-        if(inputValue === 'cancel') {
-          this.resetCalc();
-          return;
-        }else if('+-*÷=mod**'.indexOf(inputValue) >= 0) {
-          this.calculate(inputValue, calcInput.value);
-          return;
-        } else {
-          calcInput.value += e.currentTarget.dataset['input'];
-        }
-        
-      })
-    })
-
-    const allowed = /^[0-9+\-*./]*$/;
-    calcInput.addEventListener("input", function () {
-      if (!allowed.test(this.value)) {
-        this.value = this.value.replace(/[^0-9+\-*/]/g, "");
-      }
-    })
-  }
 
   resetCalc() {
     const calcInput = document.getElementById('calc-input');
@@ -63,12 +29,14 @@ export class Calculator {
     const calcInput = document.getElementById('calc-input');
     const calcHistory = document.getElementById('calc-history');
     if(this.#previousValue === null) {
+      if(this.#op === null && op === '=') return;
+      if(this.#previousValue === null && val === '') return;
       this.#previousValue = +val;
       this.#op = op;
       calcHistory.textContent = `${val} ${op}`
       calcInput.value = '';
     } else {
-      let calcValue = this.methods(this.#op, this.#previousValue, +val);
+      let calcValue = this.#methods(this.#op, this.#previousValue, +val);
       calcValue = Math.round(calcValue * 100) / 100;
       if(op === '=') {  
         calcHistory.textContent = `${this.#previousValue} ${this.#op} ${val} =`;
--- index.js
+++ index.js
@@ -2,4 +2,35 @@ import { Calculator } from "./Calculator.js";
 
 const calculator = new Calculator;
 
-calculator.initialize();
+const calcButtons = document.querySelectorAll('button');
+const calcInput = document.getElementById('calc-input');
+
+calcButtons.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    const inputValue = e.currentTarget.dataset['input'];
+    if(inputValue === 'cancel') {
+      calculator.resetCalc();
+    }else if('+-*/=mod**'.indexOf(inputValue) >= 0) {
+      calculator.calculate(inputValue, calcInput.value);
+    } else {
+      calcInput.value += e.currentTarget.dataset['input'];
+    }
+  })
+})
+
+const windowAllowed = /^[0-9+=\-*/]*$/;
+window.addEventListener('keydown', e => {
+  const focusedElement = document.activeElement.getAttribute('id');
+  if (windowAllowed.test(e.key) && focusedElement !== 'calc-input') {
+    const inputValue = e.key;
+    if(inputValue === 'cancel') {
+      calculator.resetCalc();
+    }else if('+-*/=mod**'.indexOf(inputValue) >= 0) {
+      calculator.calculate(inputValue, calcInput.value);
+    } else {
+      calcInput.value += e.key;
+    }
+  } else if(e.key === 'Backspace') {
+    calcInput.value = calcInput.value.slice(0, calcInput.value.length - 1);
+  }
+})
